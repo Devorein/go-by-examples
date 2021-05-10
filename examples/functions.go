a621d479f33942f9ef98b7963b2f537051a93416
Safwan Shaheer <devorein00@gmail.com>
2021-05-10 18:15:38 +0600

    Updated functions package

--- examples/functions.go
+++ examples/functions.go
@@ -19,13 +19,31 @@ func fn2(a int, b string) string {
 	return strconv.Itoa(a) + b
 }
 
+// Function returning tuples
+func fn3() (int, int) {
+	return 3, 6
+}
+
 func Functions() {
-	res := plus(1, 2, 3)
-	fmt.Println(res)
+	res1 := plus(1, 2, 3)
+	fmt.Println(res1)
+
+	res2 := plus([]int{1, 3, 4}...)
+	fmt.Println(res2)
 
 	// Functions stored as value to variables
 	fn := plus
 	fmt.Println(fn(1, 2, 3))
 
 	fmt.Println(fn2(1, "a"))
+
+	// Initializing variables using function that returns a tuple
+	a, b := fn3()
+	fmt.Println(a, b)
+
+	fmt.Println(fn3())
+
+	// only extracting required values from a function that returns a tuple
+	_, c := fn3()
+	fmt.Println(c)
 }
