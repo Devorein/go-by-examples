3fc5d009099a88e2713be7f215c404b563ca01eb
Safwan Shaheer <devorein00@gmail.com>
2021-05-10 16:20:57 +0600

    Added constants package

---
+++ examples/constants.go
@@ -0,0 +1,21 @@
+package examples
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func Constants() {
+	fmt.Println(s)
+
+	const n = 500000000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+}
--- main/main.go
+++ main/main.go
@@ -6,4 +6,5 @@ func main() {
 	examples.HelloWorld()
 	examples.Values()
 	examples.Variables()
+	examples.Constants()
 }
